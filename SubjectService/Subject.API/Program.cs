4a9121588fcfa1b60a29995b46ca4138b73ef2d0
Matias Møller Hansen <mmha59221@edu.ucl.dk>
2025-02-24 18:15:22 +0100

    Aspire re-organisering (mappe strukturen fucked up?)

--- SubjectService/Subject.API/Program.cs
+++ SubjectService/Subject.API/Program.cs
@@ -1,4 +1,3 @@
-using Dapr.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,21 +22,21 @@ app.UseHttpsRedirection();
 // --------------- EndPoints ----------------
 app.MapGet("/", () => "Hello from SubjectService");
 
-app.MapGet("/PreviewMetadata/{file_name}", async (string file_name) =>
-{
-    // Opret en DaprClient for service invocation
-    var daprClient = new DaprClientBuilder().Build();
+//app.MapGet("/PreviewMetadata/{file_name}", async (string file_name) =>
+//{
+//    // Opret en DaprClient for service invocation
+//    var daprClient = new DaprClientBuilder().Build();
 
-    // Kald PythonC3DReader via Dapr. 
-    // "pythonc3dreader" er Dapr-app id for PythonC3DReader (som defineret i din Program.cs)
-    // Vi kalder /metadata/{file_name} metoden.
-    var metadata = await daprClient.InvokeMethodAsync<object>(
-        HttpMethod.Get,
-        "pythonc3dreader",
-        $"metadata/{file_name}");
+//    // Kald PythonC3DReader via Dapr. 
+//    // "pythonc3dreader" er Dapr-app id for PythonC3DReader (som defineret i din Program.cs)
+//    // Vi kalder /metadata/{file_name} metoden.
+//    var metadata = await daprClient.InvokeMethodAsync<object>(
+//        HttpMethod.Get,
+//        "pythonc3dreader",
+//        $"metadata/{file_name}");
 
-    return Results.Ok(metadata);
-});
+//    return Results.Ok(metadata);
+//});
 
 
 app.Run();
